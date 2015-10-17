15b8c67111d172c50b30fcc2a0ae280fb540e798
iyahoo <s1200191@gmail.com>
2015-10-17 19:50:53 +0900

    [Add] make-asserts, with-dbc

--- t/cl-dbc-lclj.lisp
+++ t/cl-dbc-lclj.lisp
@@ -9,10 +9,34 @@
 
 (plan nil)
 
-(is-expand (cl-dbc-lclj::make-asserts ((not (zerop 1)) (numberp 1)))
-           (PROGN
-             (ASSERT (NOT (ZEROP 1)))
-             (ASSERT (NUMBERP 1))))
+(subtest "make-asserts"
+  (is-expand (cl-dbc-lclj::make-asserts ((not (zerop 1)) (numberp 1)))
+             (progn
+               (assert (not (zerop 1)))
+               (assert (numberp 1))))
+  (is-expand (cl-dbc-lclj::make-asserts t)
+             nil))
+
+(subtest "with-dbc"
+  (is-expand (cl-dbc-lclj::with-dbc
+                 (:pre  ((not (zerop n)) (numberp n))
+                  :post ((plusp %) (numberp %)))
+               (* n n))
+             (progn
+               (let (($ (match (:pre ((not (zerop n)) (numberp n))
+                                :post ((plusp %) (numberp %)))
+                          ((property :pre pre-conds) pre-conds)
+                          (otherwise t)))
+                     ($ (match (:pre ((not (zerop n)) (numberp n))
+                                :post ((plusp %) (numberp %)))
+                          ((property :post post-conds) post-conds)
+                          (otherwise t))))
+                 (make-asserts $)
+                 (funcall #'(lambda (%)
+                              (make-asserts $)
+                              %)
+                          (progn
+                            (* n n)))))))
 
 
 (finalize)
