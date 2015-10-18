765a2f2db3003dcb17bd5432ab62f4eb7aec13a6
iyahoo <s1200191@gmail.com>
2015-10-19 00:50:23 +0900

    [Fix] with-dbc

--- src/cl-dbc-lclj.lisp
+++ src/cl-dbc-lclj.lisp
@@ -1,31 +1,12 @@
 (in-package :cl-user)
 (defpackage cl-dbc-lclj
   (:use :cl
-   :optima
-   :alexandria))
+        :optima)
+  (:import-from :alexandria
+                :with-gensyms))
 (in-package :cl-dbc-lclj)
 
-;; (defmacro with-dbc (pre post &body body)
-;;   `(progn
-;;      (assert (and ,@pre))
-;;      (funcall #'(lambda (%)
-;;                   (assert (and ,@post))
-;;                   %)
-;;               (progn
-;;                 ,@body))))
-
-;; (with-dbc
-;;     ((not (zerop n)) (numberp n))
-;;     ((plusp %) (numberp %))
-;;   (* n n))
-;;
-;; =>
-;; (PROGN
-;;   (ASSERT (AND (NOT (ZEROP N)) (NUMBERP N)))
-;;   (FUNCALL #'(LAMBDA (%)
-;;                (ASSERT (AND (PLUSP %) (NUMBERP %)))
-;;                %)
-;;            (PROGN (* N N))))
+(cl-syntax:use-syntax :annot)
 
 (defmacro make-asserts (asserts)
   (cond ((consp asserts)
@@ -34,20 +15,18 @@
                     `(assert ,a))))
         (t nil)))
 
-;; Don't work
+@export
 (defmacro with-dbc (conds &body body)
-  (with-gensyms (pre post)
+  (let ((pre (match conds
+               ((property :pre pre-conds) pre-conds)
+               (otherwise t)))
+        (post (match conds
+                ((property :post post-conds) post-conds)
+                (otherwise t))))
     `(progn
-       (let ((,pre (match ,conds
-                     ((property :pre pre-conds) pre-conds)
-                     (otherwise t)))
-             (,post (match ,conds
-                      ((property :post post-conds) post-conds)
-                      (otherwise t)))))
-       ,(print pre)
-       ,(make-asserts pre)
+       (make-asserts ,pre)
        (funcall #'(lambda (%)
-                    ,(make-asserts post)
+                    (make-asserts ,post)
                     %)
                 (progn
                   ,@body)))))
@@ -59,15 +38,11 @@
 ;;
 ;; =>
 ;; (progn
-;;   (assert (not (zerop n)))
-;;   (assert (numberp n))
+;;   (progn
+;;     (assert (not (zerop n)))
+;;     (assert (numberp n)))
 ;;   (funcall #'(lambda (%)
-;;                (assert (plusp %))
-;;                (assert (numberp %)))
+;;                (progn
+;;                  (assert (plusp %))
+;;                  (assert (numberp %))))
 ;;            (* n n)))
-
-(defun test-add (x y)
-  (with-dbc
-      ((not (= x 0)) (< y 100) (and (oddp x) (evenp y)))
-      ((not (= 0 %)))
-    (+ x y)))
