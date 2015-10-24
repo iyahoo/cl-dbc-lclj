07d5f0ecde801c48a51a800eda0729b9877846e5
iyahoo <s1200191@gmail.com>
2015-10-24 20:53:06 +0900

    [close #2] don't use optima [Add] fun property
    Fnuction property like property of optima.

--- src/cl-dbc-lclj.lisp
+++ src/cl-dbc-lclj.lisp
@@ -1,7 +1,6 @@
 (in-package :cl-user)
 (defpackage cl-dbc-lclj
-  (:use :cl
-        :optima))
+  (:use :cl))
 (in-package :cl-dbc-lclj)
 
 (cl-syntax:use-syntax :annot)
@@ -14,15 +13,16 @@
                 :collect `(assert ,a))))
     (t nil)))
 
+(defun property (key lst)
+  (let* ((pos (position key lst)))
+    (when (numberp pos)
+      (car (nthcdr (1+ pos) lst)))))
+
 @export
 (defmacro with-dbc (conds &body body)
   "stands for with design by contract."
-  (let ((pre (match conds
-               ((property :pre pre-conds) pre-conds)
-               (otherwise t)))
-        (post (match conds
-                ((property :post post-conds) post-conds)
-                (otherwise t))))
+  (let ((pre (property :pre conds))
+        (post (property :post conds)))
     `(progn
        (make-asserts ,pre)
        (funcall #'(lambda (%)
